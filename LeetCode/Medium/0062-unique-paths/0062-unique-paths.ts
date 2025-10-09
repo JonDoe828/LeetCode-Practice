aae6bff058406bff50aa49fe2fe020b93b0141ba
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-10-09 21:16:51 +1100

    Time: 0 ms (100%), Space: 55.3 MB (65.51%) - LeetHub

--- LeetCode/Medium/0062-unique-paths/0062-unique-paths.ts
+++ LeetCode/Medium/0062-unique-paths/0062-unique-paths.ts
@@ -1,5 +1,5 @@
 function uniquePaths(m: number, n: number): number {
-    const dp = Array.from({ length: m }, () => Array(n).fill(1));
+    const dp: number[][] = Array.from({ length: m }, () => Array(n).fill(1));
     for (let i = 1; i < m; i++) {
         for (let j = 1; j < n; j++) {
             dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
