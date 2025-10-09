68818cd5d6fc980c0e129b3e764a84626dc47662
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-10-09 23:09:44 +1100

    Time: 0 ms (100%), Space: 53.9 MB (92.23%) - LeetHub

---
+++ LeetCode/Medium/0096-unique-binary-search-trees/0096-unique-binary-search-trees.ts
@@ -0,0 +1,10 @@
+function numTrees(n: number): number {
+    const G = Array(n + 1).fill(0);
+    G[0] = 1; G[1] = 1;
+    for (let m = 2; m <= n; m++) {
+        for (let root = 1; root <= m; root++) {
+            G[m] += G[root - 1] * G[m - root];
+        }
+    }
+    return G[n];
+};
