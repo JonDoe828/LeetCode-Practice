c6b4efe974757631bcdc90ca96e6ff760b6dc128
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-10-20 07:46:27 +1100

    Time: 0 ms (100%), Space: 7.9 MB (78.27%) - LeetHub

--- LeetCode/Medium/0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ LeetCode/Medium/0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,13 +1,24 @@
+// class Solution {
+// public:
+//     int numTrees(int n) {
+//         vector<long long> G(n + 1, 0);
+//         G[0] = G[1] = 1;
+//         for (int m = 2; m <= n; ++m) {
+//             for (int root = 1; root <= m; ++root) {
+//                 G[m] += G[root - 1] * G[m - root];
+//             }
+//         }
+//         return (int)G[n];
+//     }
+// };
+
 class Solution {
 public:
     int numTrees(int n) {
-        vector<long long> G(n + 1, 0);
-        G[0] = G[1] = 1;
-        for (int m = 2; m <= n; ++m) {
-            for (int root = 1; root <= m; ++root) {
-                G[m] += G[root - 1] * G[m - root];
-            }
+        long long c = 1;
+        for (int k = 1; k < n; k++) {
+            c = 2 * c * (2 * k + 1) / (k + 2);
         }
-        return (int)G[n];        
+        return c;
     }
 };
