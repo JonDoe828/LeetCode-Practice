0a6878b6fe2fcaf8495f6bc7bdd50d97e612b2ee
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-10-20 21:30:33 +1100

    Time: 41 ms (94.65%), Space: 18.6 MB (54.7%) - LeetHub

---
+++ LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+
+        if (sum % 2 == 1)
+            return false;
+
+        int target = sum / 2;
+
+        vector<int> dp(10001, 0);
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = target; j >= nums[i]; j--) {
+                dp[j] = max(dp[j], dp[j - nums[i]] + nums[i]);
+            }
+        }
+        // 集合中的元素正好可以凑成总和target
+        if (dp[target] == target)
+            return true;
+        return false;
+    }
+};
