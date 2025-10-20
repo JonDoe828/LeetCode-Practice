41614c9e863e8954beca7a3d9897ec083e7b9bfa
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-10-20 18:35:55 +1100

    Time: 63 ms (68.12%), Space: 56.2 MB (97.89%) - LeetHub

--- LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
+++ LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
@@ -5,9 +5,10 @@
 var canPartition = function (nums) {
     const sum = nums.reduce((a, b) => a + b, 0);
     if (sum % 2 !== 0) return false;
-    const target = sum / 2;
 
+    const target = sum / 2;
     const dp = new Array(target + 1).fill(0);
+    
     for (const x of nums) {
         for (let j = target; j >= x; j--) {
             dp[j] = Math.max(dp[j], dp[j - x] + x);
