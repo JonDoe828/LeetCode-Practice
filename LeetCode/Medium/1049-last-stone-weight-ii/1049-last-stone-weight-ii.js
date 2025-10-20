884baa40990a4013a59b5767fff8c2cb5f1bd444
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-10-21 08:53:00 +1100

    Time: 0 ms (100%), Space: 53.9 MB (95.45%) - LeetHub

--- LeetCode/Medium/1049-last-stone-weight-ii/1049-last-stone-weight-ii.js
+++ LeetCode/Medium/1049-last-stone-weight-ii/1049-last-stone-weight-ii.js
@@ -2,20 +2,35 @@
  * @param {number[]} stones
  * @return {number}
  */
-var lastStoneWeightII = function (stones) {
-    const sum = stones.reduce((a, b) => a + b, 0)
-    const target = Math.floor(sum / 2); // 只取一半及以下
-    const dp = new Array(target + 1).fill(false);
-    dp[0] = true;
+// var lastStoneWeightII = function (stones) {
+//     const sum = stones.reduce((a, b) => a + b, 0)
+//     const target = Math.floor(sum / 2); // 只取一半及以下
+//     const dp = new Array(target + 1).fill(false);
+//     dp[0] = true;
 
-    for (const x of stones) {
-        for (let w = target; w >= x; w--) {
-            dp[w] = dp[w] || dp[w - x];
-        }
-    }
+//     for (const x of stones) {
+//         for (let w = target; w >= x; w--) {
+//             dp[w] = dp[w] || dp[w - x];
+//         }
+//     }
 
-    for (let s = target; s >= 0; s--) {
-        if (dp[s]) return sum - 2 * s;
-    }
-    return 0;
+//     for (let s = target; s >= 0; s--) {
+//         if (dp[s]) return sum - 2 * s;
+//     }
+//     return 0;
+// };
+
+
+var lastStoneWeightII = function(stones) {
+  const sum = stones.reduce((a, b) => a + b, 0);
+  const target = Math.floor(sum / 2);
+  let bits = 1n; // 只有和为0可达
+  for (const x of stones) {
+    bits |= (bits << BigInt(x));
+  }
+  // 从 target 往下找第一个可达的 s
+  for (let s = target; s >= 0; s--) {
+    if ((bits >> BigInt(s)) & 1n) return sum - 2 * s;
+  }
+  return 0;
 };
