4480a553ee17f6aed1c09e7023d0007bdd4ff0a6
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-10-22 17:15:06 +1100

    Time: 0 ms (100%), Space: 8.3 MB (95.75%) - LeetHub

--- LeetCode/Medium/0279-perfect-squares/0279-perfect-squares.cpp
+++ LeetCode/Medium/0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,13 +1,35 @@
+// class Solution {
+// public:
+//     int numSquares(int n) {
+//         std::vector<int> dp(n + 1, n); 
+//         dp[0] = 0;
+//         for (int i = 1; i <= n; ++i) {
+//             for (int j = 1; j * j <= i; ++j) {
+//                 dp[i] = std::min(dp[i], dp[i - j * j] + 1);
+//             }
+//         }
+//         return dp[n];
+//     }
+// };
+
+
+
 class Solution {
+    static bool isSquare(int x) {
+        int r = (int)std::sqrt(x);
+        return r * r == x;
+    }
 public:
     int numSquares(int n) {
-        std::vector<int> dp(n + 1, n); 
-        dp[0] = 0;
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j * j <= i; ++j) {
-                dp[i] = std::min(dp[i], dp[i - j * j] + 1);
-            }
+        if (isSquare(n)) return 1;
+        // 去掉 4 的因子
+        int m = n;
+        while ((m & 3) == 0) m >>= 2;
+        if (m % 8 == 7) return 4;
+        // 是否为两个平方和
+        for (int a = 1; a * a <= n; ++a) {
+            if (isSquare(n - a * a)) return 2;
         }
-        return dp[n];
+        return 3;
     }
 };
