eaa28f7fa8650c4d3bc4824d8efafdb98f05ac1d
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-10-22 13:15:07 +1100

    Time: 3 ms (96.32%), Space: 10.1 MB (94.95%) - LeetHub

--- LeetCode/Medium/0518-coin-change-ii/0518-coin-change-ii.cpp
+++ LeetCode/Medium/0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        using u128 = __uint128_t;              // GNU 扩展，LeetCode/Clang/GCC 通常可用
-        vector<u128> dp(amount + 1, 0);
+        vector<int> dp(amount + 1, 0);
         dp[0] = 1;
-        for (int c : coins) {
-            for (int x = c; x <= amount; ++x) {
-                dp[x] += dp[x - c];
+        for (int i = 0; i < coins.size(); i++) {       // 遍历物品
+            for (int j = coins[i]; j <= amount; j++) { // 遍历背包
+                if (dp[j] <= INT_MAX - dp[j - coins[i]]){
+                    dp[j] += dp[j - coins[i]];
+                }
+                
             }
         }
-        // 按题面返回 int；若你在自定义大数据上跑，可能会超 int
-        if (dp[amount] > static_cast<u128>(numeric_limits<int>::max())) {
-            // 你可以选择：返回 -1 / 截断 / 改函数签名
-            // 这里示例：截断（与 LeetCode 答案类型兼容）
-        }
-        return static_cast<int>(dp[amount]);
+        return dp[amount];
     }
 };
