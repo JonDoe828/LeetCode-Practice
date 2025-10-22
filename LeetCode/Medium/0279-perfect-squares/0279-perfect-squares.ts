6a0289e9ccc2872c6570224a0d4b576183a12494
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-10-22 17:07:29 +1100

    Time: 3 ms (96%), Space: 55.8 MB (100%) - LeetHub

--- LeetCode/Medium/0279-perfect-squares/0279-perfect-squares.ts
+++ LeetCode/Medium/0279-perfect-squares/0279-perfect-squares.ts
@@ -1,10 +1,29 @@
+// function numSquares(n: number): number {
+//     const dp = new Array<number>(n + 1).fill(n);
+//     dp[0] = 0;
+//     for (let i = 1; i <= n; i++) {
+//         for (let j = 1; j * j <= i; j++) {
+//             dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+//         }
+//     }
+//     return dp[n];
+// };
+
+
 function numSquares(n: number): number {
-    const dp = new Array<number>(n + 1).fill(n);
-    dp[0] = 0;
-    for (let i = 1; i <= n; i++) {
-        for (let j = 1; j * j <= i; j++) {
-            dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
-        }
-    }
-    return dp[n];
-};
+  const isSquare = (x: number) => {
+    const r = Math.trunc(Math.sqrt(x));
+    return r * r === x;
+  };
+
+  if (isSquare(n)) return 1;
+
+  let m = n;
+  while ((m & 3) === 0) m >>= 2; // 去掉4因子
+  if (m % 8 === 7) return 4;
+
+  for (let a = 1; a * a <= n; a++) {
+    if (isSquare(n - a * a)) return 2;
+  }
+  return 3;
+}
