81b531b1b3dc376e0406010f989692007cdb4173
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-10-22 16:08:34 +1100

    Time: 14 ms (94.07%), Space: 18 MB (71.95%) - LeetHub

--- LeetCode/Medium/0322-coin-change/0322-coin-change.cpp
+++ LeetCode/Medium/0322-coin-change/0322-coin-change.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        if (amount == 0) return 0;
+        // if (amount == 0)
+        //     return 0;
         const int INF = amount + 1;
         vector<int> dp(amount + 1, INF);
         dp[0] = 0;
 
         for (int i = 0; i < coins.size(); i++) {       // 遍历物品
             for (int j = coins[i]; j <= amount; j++) { // 遍历背包
-                
-                    dp[j] = min(dp[j - coins[i]] + 1, dp[j]);
-                
+                dp[j] = min(dp[j - coins[i]] + 1, dp[j]);
             }
         }
         return dp[amount] == INF ? -1 : dp[amount];
