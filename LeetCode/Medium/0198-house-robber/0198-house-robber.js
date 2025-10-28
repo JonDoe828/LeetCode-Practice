093cdd78d5e01d3b2093a63b772b6a15ac2ad76c
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-10-28 17:20:26 +1100

    Time: 0 ms (100%), Space: 55.5 MB (5.81%) - LeetHub

--- LeetCode/Medium/0198-house-robber/0198-house-robber.js
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var rob = function (nums) {
-    dp = new Array(nums.length).fill(0)
+    const dp = new Array(nums.length).fill(0)
     dp[0] = nums[0]
     dp[1] = Math.max(nums[0], nums[1])
     for (let i = 2; i < nums.length; i++) {
