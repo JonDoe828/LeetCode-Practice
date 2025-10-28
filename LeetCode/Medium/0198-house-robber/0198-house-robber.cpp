04889e5e0ae04b31d7d97f02dc3da7035dacd306
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-10-28 17:31:08 +1100

    Time: 0 ms (100%), Space: 10.7 MB (46.57%) - LeetHub

--- LeetCode/Medium/0198-house-robber/0198-house-robber.cpp
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if (nums.size() == 0)
+        int n = (int)nums.size();
+        if (n == 0)
             return 0;
-        if (nums.size() == 1)
+        if (n == 1)
             return nums[0];
-        vector<int> dp(nums.size(), 0);
+
+        vector<int> dp(n, 0);
         dp[0] = nums[0];
         dp[1] = max(nums[0], nums[1]);
-        for (int i = 2; i < nums.size(); i++) {
-            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+
+        for (int i = 2; i < n; ++i) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
         }
-        return dp[nums.size() - 1];
+        return dp[n - 1];
     }
 };
