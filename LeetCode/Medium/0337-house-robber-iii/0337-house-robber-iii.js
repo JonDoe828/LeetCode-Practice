fb131909ad6ac6cb9230e50d3d826fc38ef9dfb4
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-10-30 21:08:50 +1100

    Time: 13 ms (6.91%), Space: 61.5 MB (9.04%) - LeetHub

--- LeetCode/Medium/0337-house-robber-iii/0337-house-robber-iii.js
+++ LeetCode/Medium/0337-house-robber-iii/0337-house-robber-iii.js
@@ -10,15 +10,36 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var rob = function (root) {
-    function dfs(node) {
-        if (!node) return [0, 0]; // [rob, skip]
-        const [lrob, lskip] = dfs(node.left);
-        const [rrob, rskip] = dfs(node.right);
-        const rob = node.val + lskip + rskip;
-        const skip = Math.max(lrob, lskip) + Math.max(rrob, rskip);
-        return [rob, skip];
+// var rob = function (root) {
+//     function dfs(node) {
+//         if (!node) return [0, 0]; // [rob, skip]
+//         const [lrob, lskip] = dfs(node.left);
+//         const [rrob, rskip] = dfs(node.right);
+//         const rob = node.val + lskip + rskip;
+//         const skip = Math.max(lrob, lskip) + Math.max(rrob, rskip);
+//         return [rob, skip];
+//     }
+//     const [r, s] = dfs(root);
+//     return Math.max(r, s);
+// };
+
+
+var rob = function(root){
+  const robM = new WeakMap(), skipM = new WeakMap();
+  const st = [[root,0]];
+  let ans = 0;
+  while(st.length){
+    const [n,f]=st.pop();
+    if(!n) continue;
+    if(!f){ st.push([n,1]); if(n.right) st.push([n.right,0]); if(n.left) st.push([n.left,0]); }
+    else{
+      const lrob= robM.get(n.left)||0,  lskip= skipM.get(n.left)||0;
+      const rrob= robM.get(n.right)||0, rskip= skipM.get(n.right)||0;
+      const rob  = n.val + lskip + rskip;
+      const skip = Math.max(lrob,lskip) + Math.max(rrob,rskip);
+      robM.set(n, rob); skipM.set(n, skip);
+      ans = Math.max(rob, skip);
     }
-    const [r, s] = dfs(root);
-    return Math.max(r, s);
+  }
+  return ans;
 };
