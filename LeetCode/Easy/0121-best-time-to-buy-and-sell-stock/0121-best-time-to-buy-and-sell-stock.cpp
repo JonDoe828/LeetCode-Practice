b5eb115343f3abd44e3b24b7d3df5da64a78a7a5
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-10-31 22:12:46 +1100

    Time: 3 ms (27.92%), Space: 97.5 MB (7.2%) - LeetHub

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,17 +1,34 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int len = prices.size();
+//         if (len == 0)
+//             return 0;
+//         vector<vector<int>> dp(len, vector<int>(2));
+//         dp[0][0] -= prices[0];
+//         dp[0][1] = 0;
+
+//         for (int i = 1; i < len; i++) {
+//             dp[i][0] = max(dp[i - 1][0], -prices[i]);
+//             dp[i][1] = max(dp[i - 1][1], prices[i] + dp[i - 1][0]);
+//         }
+//         return dp[len - 1][1];
+//     }
+// };
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int len = prices.size();
-        if (len == 0)
-            return 0;
-        vector<vector<int>> dp(len, vector<int>(2));
+        vector<vector<int>> dp(
+            2, vector<int>(2)); // 注意这⾥只开辟了⼀个2 * 2⼤⼩的⼆维数组
         dp[0][0] -= prices[0];
         dp[0][1] = 0;
-
         for (int i = 1; i < len; i++) {
-            dp[i][0] = max(dp[i - 1][0], -prices[i]);
-            dp[i][1] = max(dp[i - 1][1], prices[i] + dp[i - 1][0]);
+            dp[i % 2][0] = max(dp[(i - 1) % 2][0], -prices[i]);
+            dp[i % 2][1] =
+                max(dp[(i - 1) % 2][1], prices[i] + dp[(i - 1) % 2][0]);
         }
-        return dp[len - 1][1];
+        return dp[(len - 1) % 2][1];
     }
 };
