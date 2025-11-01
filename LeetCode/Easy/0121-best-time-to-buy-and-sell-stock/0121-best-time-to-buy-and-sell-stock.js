2d7b95996d117c4c142148498d796acae99b7e4b
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-11-01 12:51:12 +1100

    Time: 1 ms (96.09%), Space: 65.9 MB (41.47%) - LeetHub

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -31,7 +31,7 @@
 
 
 
-var maxProfit = function (prices) {
+const maxProfit = (prices) => {
     if (prices.length === 0) return 0;
 
     let buy = prices[0];
@@ -46,19 +46,3 @@ var maxProfit = function (prices) {
     return profit;
 }
 
-
-// const maxProfit = (prices) => {
-//   let left = 0; // Buy
-//   let right = 1; // Sell
-//   let max_profit = 0;
-//   while (right < prices.length) {
-//     if (prices[left] < prices[right]) {
-//       let profit = prices[right] - prices[left];
-//       max_profit = Math.max(max_profit, profit);
-//     } else {
-//       left = right;
-//     }
-//     right++;
-//   }
-//   return max_profit;
-// };
