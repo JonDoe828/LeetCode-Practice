07831df76dcb822ba75b9b9290d91c9b2ad21bc2
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-11-01 11:15:33 +1100

    Time: 0 ms (100%), Space: 65.2 MB (93.3%) - LeetHub

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,28 +1,28 @@
-// function maxProfit(prices: number[]): number {
-//     if (prices.length === 0) return 0;
+function maxProfit(prices: number[]): number {
+    if (prices.length === 0) return 0;
 
-//     let buy = prices[0];
-//     let profit = 0;
-//     for (let i = 1; i < prices.length; i++) {
-//         if (prices[i] < buy) {
-//             buy = prices[i];
-//         } else if (prices[i] - buy > profit) {
-//             profit = prices[i] - buy;
-//         }
-//     }
-//     return profit;
-// };
+    let buy = prices[0];
+    let profit = 0;
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < buy) {
+            buy = prices[i];
+        } else if (prices[i] - buy > profit) {
+            profit = prices[i] - buy;
+        }
+    }
+    return profit;
+};
 
 
-export function maxProfit(prices: number[]): number {
-  if (prices.length === 0) return 0;
+// export function maxProfit(prices: number[]): number {
+//   if (prices.length === 0) return 0;
 
-  let minPrice = prices[0];
-  let best = 0;
+//   let minPrice = prices[0];
+//   let best = 0;
 
-  for (let i = 1; i < prices.length; i++) {
-    minPrice = Math.min(minPrice, prices[i]);
-    best = Math.max(best, prices[i] - minPrice);
-  }
-  return best;
-}
+//   for (let i = 1; i < prices.length; i++) {
+//     minPrice = Math.min(minPrice, prices[i]);
+//     best = Math.max(best, prices[i] - minPrice);
+//   }
+//   return best;
+// }
