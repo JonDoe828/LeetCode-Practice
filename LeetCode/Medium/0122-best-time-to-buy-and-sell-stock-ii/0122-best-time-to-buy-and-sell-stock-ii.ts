88ff5833622ca09c07686b61a101c9f28b15b777
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-11-01 18:29:16 +1100

    Time: 1 ms (71.88%), Space: 55 MB (89.42%) - LeetHub

--- LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.ts
+++ LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.ts
@@ -1,9 +1,7 @@
 function maxProfit(prices: number[]): number {
     let profit = 0;
     for (let i = 1; i < prices.length; i++) {
-        if (prices[i] > prices[i - 1]) {
-            profit += prices[i] - prices[i - 1];
-        }
+        if (prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
     }
     return profit;
 };
