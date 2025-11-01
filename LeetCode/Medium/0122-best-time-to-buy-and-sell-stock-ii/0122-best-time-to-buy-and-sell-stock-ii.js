b55c9434cb0f3f485057f5c6f007f0bace8bbe3f
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-11-01 18:20:41 +1100

    Time: 4 ms (9.36%), Space: 62.5 MB (5.09%) - LeetHub

--- LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -4,18 +4,18 @@
  */
 
 //2D DP
-// var maxProfit = function (prices) {
-//     const len = prices.length
-//     if (len == 0) return 0
-//     const dp = Array.from({ length: len }, () => [0, 0]);
-//     dp[0][0] = - prices[0]
-//     dp[0][1] = 0
-//     for (let i = 1; i < len; i++) {
-//         dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i])
-//         dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i])
-//     }
-//     return dp[len - 1][1]
-// };
+var maxProfit = function (prices) {
+    const len = prices.length
+    if (len == 0) return 0
+    const dp = Array.from({ length: len }, () => [0, 0]);
+    dp[0][0] = - prices[0]
+    dp[0][1] = 0
+    for (let i = 1; i < len; i++) {
+        dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i])
+        dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i])
+    }
+    return dp[len - 1][1]
+};
 
 
 //3ms
@@ -48,14 +48,14 @@
 
 
 //0 ms
-var maxProfit = function (prices) {
-  if (prices.length === 0) return 0;
+// var maxProfit = function (prices) {
+//   if (prices.length === 0) return 0;
 
-  let profit = 0;
-  for (let i = 1; i < prices.length; i++) {
-    if (prices[i] > prices[i - 1]) {
-      profit += prices[i] - prices[i - 1]; // 吃下每一步的上涨
-    }
-  }
-  return profit;
-};
+//   let profit = 0;
+//   for (let i = 1; i < prices.length; i++) {
+//     if (prices[i] > prices[i - 1]) {
+//       profit += prices[i] - prices[i - 1]; // 吃下每一步的上涨
+//     }
+//   }
+//   return profit;
+// };
