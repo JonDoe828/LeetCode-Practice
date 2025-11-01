db3c7fcb937815a93f1e66fc1f46a01a20f3c387
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-11-01 12:52:23 +1100

    Time: 158 ms (5.63%), Space: 93.1 MB (5.5%) - LeetHub

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -4,30 +4,30 @@
  */
 
 //2D DP
-// var maxProfit = function (prices) {
-//     const len = prices.length
-//     if (len == 0) return 0
-//     const dp = Array.from({ length: len }, () => [0, 0]);
-//     dp[0][0] = - prices[0]
-//     dp[0][1] = 0
-//     for (let i = 1; i < len; i++) {
-//         dp[i][0] = Math.max(dp[i - 1][0], -prices[i])
-//         dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i])
-//     }
-//     return dp[len - 1][1]
-// };
-
-
 var maxProfit = function (prices) {
-    let hold = -Infinity; // 持有状态（最多买一次）：相当于 -minPrice
-    let cash = 0;         // 不持有状态（已经卖出或从未买）
-
-    for (const p of prices) {
-        hold = Math.max(hold, -p);   // 选择在更低价买入
-        cash = Math.max(cash, hold + p); // 选择在今天卖出
+    const len = prices.length
+    if (len == 0) return 0
+    const dp = Array.from({ length: len }, () => [0, 0]);
+    dp[0][0] = - prices[0]
+    dp[0][1] = 0
+    for (let i = 1; i < len; i++) {
+        dp[i][0] = Math.max(dp[i - 1][0], -prices[i])
+        dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i])
     }
-    return cash;
+    return dp[len - 1][1]
 };
+
+
+// var maxProfit = function (prices) {
+//     let hold = -Infinity; // 持有状态（最多买一次）：相当于 -minPrice
+//     let cash = 0;         // 不持有状态（已经卖出或从未买）
+
+//     for (const p of prices) {
+//         hold = Math.max(hold, -p);   // 选择在更低价买入
+//         cash = Math.max(cash, hold + p); // 选择在今天卖出
+//     }
+//     return cash;
+// };
 
 
 
