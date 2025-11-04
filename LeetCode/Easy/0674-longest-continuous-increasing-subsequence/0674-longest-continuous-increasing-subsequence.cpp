d4b3d75b079f6b8f536e28fcb626b69ca2ba598f
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-11-04 19:41:16 +1100

    Time: 0 ms (100%), Space: 14.6 MB (88.85%) - LeetHub

---
+++ LeetCode/Easy/0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        if (nums.empty())
+            return 0;
+        int ans = 1, cur = 1;
+        for (int i = 1; i < (int)nums.size(); ++i) {
+            if (nums[i] > nums[i - 1])
+                ++cur;
+            else
+                cur = 1;
+            ans = max(ans, cur);
+        }
+        return ans;
+    }
+};
