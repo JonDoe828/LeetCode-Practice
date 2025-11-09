16d0d88d9c9ead4604daf7aa98e1d94318e910d4
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-11-09 17:40:36 +1100

    Time: 0 ms (100%), Space: 8.5 MB (89.95%) - LeetHub

--- LeetCode/Easy/0392-is-subsequence/0392-is-subsequence.cpp
+++ LeetCode/Easy/0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,17 +1,31 @@
+// class Solution {
+// public:
+//     bool isSubsequence(string s, string t) {
+//         int m = s.size(), n = t.size();
+//         vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+//         for (int i = 1; i <= m; ++i) {
+//             for (int j = 1; j <= n; ++j) {
+//                 if (s[i - 1] == t[j - 1]) {
+//                     dp[i][j] = dp[i - 1][j - 1] + 1;
+//                 } else {
+//                     dp[i][j] = dp[i][j - 1];
+//                 }
+//             }
+//         }
+//         return dp[m][n] == m;
+//     }
+// };
+
+
+//双指针
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int m = s.size(), n = t.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                if (s[i - 1] == t[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = dp[i][j - 1];
-                }
-            }
+        int i = 0, j = 0;
+        while (i < (int)s.size() && j < (int)t.size()) {
+            if (s[i] == t[j]) ++i;
+            ++j;
         }
-        return dp[m][n] == m;
+        return i == (int)s.size();
     }
 };
