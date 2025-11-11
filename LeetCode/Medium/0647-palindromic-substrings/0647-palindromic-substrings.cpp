ea21b7116f8d5c13fc86c9d65126323d7c92137d
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-11-12 02:27:46 +1100

    Time: 18 ms (42.68%), Space: 11.1 MB (46.6%) - LeetHub

--- LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -6,9 +6,14 @@ public:
         vector<vector<bool>> dp(n, vector<bool>(n, false));
         for (int i = n - 1; i >= 0; --i) {
             for (int j = i; j < n; ++j) {
-                if (s[i] == s[j] && (j - i < 2 || dp[i + 1][j - 1])) {
-                    dp[i][j] = true;
-                    ++ans;
+                if (s[i] == s[j]) {
+                    if (j - i <= 1) {
+                        dp[i][j] = true;
+                        ++ans;
+                    } else if (dp[i + 1][j - 1] == true) {
+                        dp[i][j] = true;
+                        ++ans;
+                    }
                 }
             }
         }
