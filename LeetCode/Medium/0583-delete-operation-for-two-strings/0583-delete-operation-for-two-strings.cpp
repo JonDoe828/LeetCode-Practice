97f23c7e3ffe906b6060949ec57e31ab384d966d
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-11-11 22:48:56 +1100

    Time: 4 ms (94.01%), Space: 11.1 MB (96.89%) - LeetHub

--- LeetCode/Medium/0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ LeetCode/Medium/0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -18,20 +18,33 @@ public:
     //     return dp[n][m];
     // }
 
+    // int minDistance(string s, string t) {
+    //     int n = s.size(), m = t.size();
+    //     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    //     for (int i = 1; i <= n; ++i) {
+    //         dp[i][0] = i; // 顺手设第0列
+    //         for (int j = 1; j <= m; ++j) {
+    //             dp[0][j] = j;
+    //             if (s[i - 1] == t[j - 1])
+    //                 dp[i][j] = dp[i - 1][j - 1];
+    //             else
+    //                 dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + 1;
+    //         }
+    //     }
+    //     return dp[n][m];
+    // }
 
-    int minDistance(string s, string t) {
-    int n = s.size(), m = t.size();
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-    // iota(dp[0].begin(), dp[0].end(), 0); 
-
-    for (int i = 1; i <= n; ++i) {
-        dp[i][0] = i;                    // 顺手设第0列
-        for (int j = 1; j <= m; ++j) {
-            dp[0][j] = j;
-            if (s[i-1] == t[j-1]) dp[i][j] = dp[i-1][j-1];
-            else dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + 1;
-        }
+    int minDistance(string& word1, string& word2) {
+        vector<vector<int>> dp(2, vector<int>(size(word2) + 1));
+        for (int i = 0; i <= size(word1); i++)
+            for (int j = 0; j <= size(word2); j++)
+                if (!i || !j)
+                    dp[i & 1][j] = i + j;
+                else
+                    dp[i & 1][j] =
+                        word1[i - 1] == word2[j - 1]
+                            ? dp[(i - 1) & 1][j - 1]
+                            : 1 + min(dp[(i - 1) & 1][j], dp[i & 1][j - 1]);
+        return dp[size(word1) & 1][size(word2)];
     }
-    return dp[n][m];
-}
 };
