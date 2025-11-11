075fae9ed356c0fc3e286a8c501f0ce570f9a975
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-11-11 22:47:40 +1100

    Time: 13 ms (42.24%), Space: 16.9 MB (39.54%) - LeetHub

--- LeetCode/Medium/0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ LeetCode/Medium/0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -22,11 +22,12 @@ public:
     int minDistance(string s, string t) {
     int n = s.size(), m = t.size();
     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-    iota(dp[0].begin(), dp[0].end(), 0); // 只这一行
+    // iota(dp[0].begin(), dp[0].end(), 0); 
 
     for (int i = 1; i <= n; ++i) {
         dp[i][0] = i;                    // 顺手设第0列
         for (int j = 1; j <= m; ++j) {
+            dp[0][j] = j;
             if (s[i-1] == t[j-1]) dp[i][j] = dp[i-1][j-1];
             else dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + 1;
         }
