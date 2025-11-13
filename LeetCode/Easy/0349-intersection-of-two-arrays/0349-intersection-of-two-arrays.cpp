241507cb979a5cfaa4cb20e50014e96ff0e9e06b
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-11-13 20:06:34 +1100

    Time: 3 ms (39.44%), Space: 14.8 MB (9.3%) - LeetHub

--- LeetCode/Easy/0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ LeetCode/Easy/0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> s1(nums1.begin(), nums1.end());
-        unordered_set<int> res;
+    // vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    //     unordered_set<int> s1(nums1.begin(), nums1.end());
+    //     unordered_set<int> res;
 
-        for (int i : nums2) {
-            if (s1.count(i)) {
-                res.insert(i);
+    //     for (int i : nums2) {
+    //         if (s1.count(i)) {
+    //             res.insert(i);
+    //         }
+    //     }
+    //     return vector<int>(res.begin(), res.end());
+    // }
+
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> res;
+        int hash[1001] = {0};
+
+        for (int num : nums1) {
+            hash[num] = 1;
+        }
+
+        for (int num : nums2) {
+            if (hash[num] == 1) {
+                res.insert(num);
             }
         }
         return vector<int>(res.begin(), res.end());
