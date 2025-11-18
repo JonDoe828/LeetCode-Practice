9366e213dc72aab15d8023fa874a54b4fff57306
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-11-19 02:03:39 +1100

    Time: 0 ms (100%), Space: 11.7 MB (47.14%) - LeetHub

---
+++ LeetCode/Easy/0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slow = 0, fast =0;
+        while(fast< nums.size()){
+            if(nums[fast]!=val){
+                nums[slow++] = nums[fast];
+            }
+            fast++;
+        }
+        return slow;
+    }
+};
