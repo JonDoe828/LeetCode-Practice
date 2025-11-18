e0edc763f7a34608b0e02455bf6ef150d73f7bb0
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-11-18 20:49:53 +1100

    Time: 0 ms (100%), Space: 27.2 MB (80.29%) - LeetHub

--- LeetCode/Easy/0344-reverse-string/0344-reverse-string.cpp
+++ LeetCode/Easy/0344-reverse-string/0344-reverse-string.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
+    // void reverseString(vector<char>& s) {
+    //     int i = 0;
+    //     int j = s.size() - 1;
+    //     while (i < j) {
+    //         swap(s[i], s[j]); // 交换左右字符
+    //         ++i;
+    //         --j;
+    //     }
+    // }
+
     void reverseString(vector<char>& s) {
-        int i = 0;
-        int j = s.size() - 1;
-        while (i < j) {
-            swap(s[i], s[j]); // 交换左右字符
-            ++i;
-            --j;
+        for (int i = 0, j = s.size() - 1; i < s.size() / 2; i++, j--) {
+            swap(s[i], s[j]);
         }
     }
 };
