2a3eca228af2d6a5d0a04ecb7cca867e744c22e4
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-11-22 23:32:24 +1100

    Time: 81 ms (57.1%), Space: 182.3 MB (29.21%) - LeetHub

---
+++ LeetCode/Medium/0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,46 @@
+class LRUCache {
+    int capacity_;
+    list<int> keyList_;
+    unordered_map<int, pair<int, list<int>::iterator>> hashMap_;
+
+    void Insert(int key, int value) {
+        keyList_.push_back(key);
+        hashMap_[key] = make_pair(value, --keyList_.end());
+    }
+
+public:
+    LRUCache(int capacity) { capacity_ = capacity; }
+
+    int get(int key) {
+        auto it = hashMap_.find(key);
+        if (it != hashMap_.end()) {
+            keyList_.erase(it->second.second);
+            keyList_.push_back(key);
+            hashMap_[key].second = (--keyList_.end());
+            return it->second.first;
+        }
+        return -1;
+    }
+
+    void put(int key, int value) {
+        if (get(key) != -1) {
+            hashMap_[key].first = value;
+            return;
+        }
+        if (hashMap_.size() < capacity_) {
+            Insert(key, value);
+        } else {
+            int removeKey = keyList_.front();
+            keyList_.pop_front();
+            hashMap_.erase(removeKey);
+            Insert(key, value);
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
