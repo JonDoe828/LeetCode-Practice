849c415a3703cc5f6f2476e691a666d4f9d72afc
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-11-22 16:45:52 +1100

    Time: 0 ms (100%), Space: 30.3 MB (63.66%) - LeetHub

--- LeetCode/Easy/0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ LeetCode/Easy/0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,20 +1,44 @@
 class Solution {
 public:
+    // vector<int> sortedSquares(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<int> res(n);
+    //     int k = n - 1;
+    //     for (int i = 0, j = k; i <= j;) {
+    //         int left = nums[i] * nums[i];
+    //         int right = nums[j] * nums[j];
+    //         if (left > right) {
+    //             res[k--] = nums[i] * nums[i];
+    //             i++;
+    //         } else {
+    //             res[k--] = nums[j] * nums[j];
+    //             j--;
+    //         }
+    //     }
+    //     return res;
+    // }
+
+
     vector<int> sortedSquares(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> res(n);
-        int k = n - 1;
-        for (int i = 0, j = k; i <= j;) {
-            int left = nums[i] * nums[i];
-            int right = nums[j] * nums[j];
-            if (left > right) {
-                res[k--] = nums[i] * nums[i];
-                i++;
-            } else {
-                res[k--] = nums[j] * nums[j];
-                j--;
-            }
+    int n = nums.size();
+    vector<int> res(n);   // 必须提前分配空间
+    int k = n - 1;
+
+    int i = 0;
+    int j = n - 1;
+
+    while (i <= j) {
+        int left = nums[i] * nums[i];
+        int right = nums[j] * nums[j];
+
+        if (left > right) {
+            res[k--] = left;
+            i++;
+        } else {
+            res[k--] = right;
+            j--;
         }
-        return res;
     }
+    return res;
+}
 };
