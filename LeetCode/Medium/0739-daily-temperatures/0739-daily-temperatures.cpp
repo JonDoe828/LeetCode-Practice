6e54b48139b4c1bb49348a0c168d4f70f9c7f972
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-12-09 04:07:48 +1100

    Time: 39 ms (9.36%), Space: 107.3 MB (41.72%) - LeetHub

--- LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.cpp
+++ LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,24 +1,41 @@
+// class Solution {
+// public:
+//     vector<int> dailyTemperatures(vector<int>& temperatures) {
+//         int n = temperatures.size();
+//         stack<int> st;
+//         vector<int> res(n, 0);
+//         st.push(0);
+
+//         for (int i = 1; i < n; i++) {
+//             if (temperatures[i] < temperatures[st.top()]) {
+//                 st.push(i);
+//             } else if (temperatures[i] == temperatures[st.top()]) {
+//                 st.push(i);
+//             } else {
+//                 while (!st.empty() &&
+//                        temperatures[i] > temperatures[st.top()]) {
+//                     res[st.top()] = i - st.top();
+//                     st.pop();
+//                 }
+//                 st.push(i);
+//             }
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n = temperatures.size();
-        stack<int> st;
-        vector<int> res(n, 0);
-        st.push(0);
-
-        for (int i = 1; i < n; i++) {
-            if (temperatures[i] < temperatures[st.top()]) {
-                st.push(i);
-            } else if (temperatures[i] == temperatures[st.top()]) {
-                st.push(i);
-            } else {
-                while (!st.empty() && temperatures[i] > temperatures[st.top()]) { 
-                    res[st.top()] = i - st.top();
-                    st.pop();
-                }
-                st.push(i);
+    vector<int> dailyTemperatures(vector<int>& T) {
+        stack<int> st; // 递增栈
+        vector<int> result(T.size(), 0);
+        for (int i = 0; i < T.size(); i++) {
+            while (!st.empty() && T[i] > T[st.top()]) { // 注意栈不能为空
+                result[st.top()] = i - st.top();
+                st.pop();
             }
+            st.push(i);
         }
-        return res;
+        return result;
     }
 };
