1e03ebc06e74059af5d08653fd5762cdc11c5382
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-12-19 01:39:38 +1100

    Time: 3 ms (50.51%), Space: 17.9 MB (83.3%) - LeetHub

--- LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -5,17 +5,17 @@ public:
         for (int x : nums)
             cnt[x]++;
 
-        // 小顶堆：按频次从小到大
+        // 小顶堆：按频次从小到大 / Min-heap
         using P = pair<int, int>; // (freq, val)
         priority_queue<P, vector<P>, greater<P>> pq;
 
         for (auto& kv : cnt) {
             pq.push({kv.second, kv.first});
-            if ((int)pq.size() > k)
+            if ((int)pq.size() > k) // 超出范围，pop掉顶部的最小值
                 pq.pop();
         }
 
-        vector<int> res;
+        vector<int> res; // 预分配空间, 注意与resize的区别，用resize会变成2k
         res.reserve(k);
         while (!pq.empty()) {
             res.push_back(pq.top().second);
