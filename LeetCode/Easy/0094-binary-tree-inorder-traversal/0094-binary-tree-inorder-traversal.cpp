2b0c898385b3d8a8dd32963ed9b73406ac5946d5
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-12-23 23:48:37 +1100

    Time: 0 ms (100%), Space: 10.9 MB (32.93%) - LeetHub

--- LeetCode/Easy/0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ LeetCode/Easy/0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -12,19 +12,39 @@
  */
 class Solution {
 public:
-    vector<int> res;
+    // vector<int> res;
 
-    void dfs(TreeNode* root) {
-        if (!root)
-            return;
+    // void dfs(TreeNode* root) {
+    //     if (!root)
+    //         return;
 
-        dfs(root->left);
-        res.push_back(root->val);
-        dfs(root->right);
-    }
+    //     dfs(root->left);
+    //     res.push_back(root->val);
+    //     dfs(root->right);
+    // }
+
+    // vector<int> inorderTraversal(TreeNode* root) {
+    //     dfs(root);
+    //     return res;
+    // }
+
+
 
     vector<int> inorderTraversal(TreeNode* root) {
-        dfs(root);
-        return res;
+    vector<int> res;
+    stack<TreeNode*> st;
+    TreeNode* cur = root;
+    while (cur || !st.empty()) {
+        if (cur) {
+            st.push(cur);
+            cur = cur->left;        // 一直向左访问
+        } else {
+            cur = st.top(); st.pop();
+            res.push_back(cur->val); // 中
+            cur = cur->right;        // 转向右子树
+        }
     }
+    return res;
+}
 };
+
