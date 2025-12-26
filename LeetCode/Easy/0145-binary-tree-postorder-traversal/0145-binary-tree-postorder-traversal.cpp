cf19abfd7b19adc38ac192d86c01fc2f0309db4d
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-12-27 10:22:38 +1100

    Time: 0 ms (100%), Space: 11 MB (72.17%) - LeetHub

--- LeetCode/Easy/0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ LeetCode/Easy/0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -10,21 +10,47 @@
  * right(right) {}
  * };
  */
+
+// 递归法
+//  class Solution {
+//  public:
+//      vector<int> res;
+
+//     void dfs(TreeNode* root) {
+//         if (!root)
+//             return;
+
+//         dfs(root->left);
+//         dfs(root->right);
+//         res.push_back(root->val);
+//     }
+
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         dfs(root);
+//         return res;
+//     }
+// };
+
+// 迭代法
+
 class Solution {
 public:
-    vector<int> res;
-
-    void dfs(TreeNode* root) {
-        if (!root)
-            return;
-
-        dfs(root->left);
-        dfs(root->right);
-        res.push_back(root->val);
-    }
-
     vector<int> postorderTraversal(TreeNode* root) {
-        dfs(root);
+        vector<int> res;
+        stack<TreeNode*> st;
+        if (!root)
+            return res;
+        st.push(root);
+        while (!st.empty()) {
+            TreeNode* node = st.top();
+            st.pop();
+            res.push_back(node->val);
+            if (node->left)
+                st.push(node->left); // 左先入栈
+            if (node->right)
+                st.push(node->right); // 右后入栈
+        }
+        reverse(res.begin(), res.end()); // 中右左 -> 左右中
         return res;
     }
 };
