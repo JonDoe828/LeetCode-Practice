b5ac702136d413642bffeeb7a5f2ffe5fccb18ff
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2025-12-29 02:45:45 +1100

    Time: 2 ms (35.06%), Space: 17.8 MB (5.91%) - LeetHub

--- LeetCode/Medium/0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ LeetCode/Medium/0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -11,32 +11,55 @@
  * };
  */
 
- //BFS（队列)
+// BFS（队列)
+// class Solution {
+// public:
+//     vector<vector<int>> levelOrder(TreeNode* root) {
+//         vector<vector<int>> res;
+//         if (!root)
+//             return res;
+
+//         queue<TreeNode*> q;
+//         q.push(root);
+
+//         while (!q.empty()) {
+//             int size = q.size(); // 当前层节点数
+//             vector<int> level;
+//             //for (int i = 0; i < size; ++i) {
+//               while (size--) {
+//                 TreeNode* node = q.front();
+//                 q.pop();
+//                 level.push_back(node->val);
+
+//                 if (node->left)
+//                     q.push(node->left);
+//                 if (node->right)
+//                     q.push(node->right);
+//             }
+//             res.push_back(level);
+//         }
+//         return res;
+//     }
+// };
+
+// DFS（递归 + 层号）
 class Solution {
 public:
+    vector<vector<int>> res;
+
+    void dfs(TreeNode* node, int depth) {
+        if (!node)
+            return;
+        if (depth == res.size())
+            res.push_back({});
+        res[depth].push_back(node->val);
+
+        dfs(node->left, depth + 1);
+        dfs(node->right, depth + 1);
+    }
+
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> res;
-        if (!root)
-            return res;
-
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            int size = q.size(); // 当前层节点数
-            vector<int> level;
-            for (int i = 0; i < size; ++i) {
-                TreeNode* node = q.front();
-                q.pop();
-                level.push_back(node->val);
-
-                if (node->left)
-                    q.push(node->left);
-                if (node->right)
-                    q.push(node->right);
-            }
-            res.push_back(level);
-        }
+        dfs(root, 0);
         return res;
     }
 };
