b7412c5c3ae80e58c64210edbc75244077add2d3
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-01-13 09:25:38 +1100

    Time: 0 ms (100%), Space: 31.3 MB (72.17%) - LeetHub

--- LeetCode/Easy/0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ LeetCode/Easy/0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -6,12 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
+    int getDepth(TreeNode* node) {
+        int depth = 0;
+        while (node) {
+            node = node->left;
+            depth++;
+        }
+        return depth;
+    }
+
     int countNodes(TreeNode* root) {
-        return root ? countNodes(root -> left) + countNodes(root -> right) + 1 : 0;
+        if (!root)
+            return 0;
+
+        int leftDepth = getDepth(root->left);
+        int rightDepth = getDepth(root->right);
+
+        if (leftDepth == rightDepth) {
+            // 左子树是满二叉树
+            return (1 << leftDepth) + countNodes(root->right);
+        } else {
+            // 右子树是满二叉树
+            return (1 << rightDepth) + countNodes(root->left);
+        }
     }
 };
