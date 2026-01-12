4701eb9fbea168aa94a46f857fd9bea6bedca701
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-01-12 11:08:56 +1100

    Time: 0 ms (100%), Space: 18.9 MB (75.46%) - LeetHub

---
+++ LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+
+// 前序递归
+//  class Solution {
+//  public:
+//      int depthMax  = 0;
+
+//     void dfs(TreeNode* node, int depth) {
+//         if (!node) return;
+
+//         // 前序：先处理当前节点
+//         depthMax  = max(depthMax , depth);
+
+//         dfs(node->left, depth + 1);
+//         dfs(node->right, depth + 1);
+//     }
+
+//     int maxDepth(TreeNode* root) {
+//         if (!root) return 0;
+//         dfs(root, 1);
+//         return depthMax ;
+//     }
+// };
+
+// 后序
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root)
+            return 0;
+
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+
+        return 1 + max(leftDepth, rightDepth);
+    }
+};
