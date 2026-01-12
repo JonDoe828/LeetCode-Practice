d9c0efc7231fa37a2796934712378195a2cf2877
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-01-13 09:28:50 +1100

    Time: 0 ms (100%), Space: 31.3 MB (41.66%) - LeetHub

--- LeetCode/Easy/0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ LeetCode/Easy/0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -10,30 +10,57 @@
  * right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int getDepth(TreeNode* node) {
+//         int depth = 0;
+//         while (node) {
+//             node = node->left;
+//             depth++;
+//         }
+//         return depth;
+//     }
+
+//     int countNodes(TreeNode* root) {
+//         if (!root)
+//             return 0;
+
+//         int leftDepth = getDepth(root->left);
+//         int rightDepth = getDepth(root->right);
+
+//         if (leftDepth == rightDepth) {
+//             // 左子树是满二叉树
+//             return (1 << leftDepth) + countNodes(root->right);
+//         } else {
+//             // 右子树是满二叉树
+//             return (1 << rightDepth) + countNodes(root->left);
+//         }
+//     }
+// };
+
+
+// // Version 1
+// class Solution {
+// private:
+//     int getNodesNum(TreeNode* cur) {
+//         if (cur == NULL) return 0;
+//         int leftNum = getNodesNum(cur->left);      // left
+//         int rightNum = getNodesNum(cur->right);    // right
+//         int treeNum = leftNum + rightNum + 1;      // root
+//         return treeNum;
+//     }
+// public:
+//     int countNodes(TreeNode* root) {
+//         return getNodesNum(root);
+//     }
+// };
+
+
+// Version 2
 class Solution {
 public:
-    int getDepth(TreeNode* node) {
-        int depth = 0;
-        while (node) {
-            node = node->left;
-            depth++;
-        }
-        return depth;
-    }
-
     int countNodes(TreeNode* root) {
-        if (!root)
-            return 0;
-
-        int leftDepth = getDepth(root->left);
-        int rightDepth = getDepth(root->right);
-
-        if (leftDepth == rightDepth) {
-            // 左子树是满二叉树
-            return (1 << leftDepth) + countNodes(root->right);
-        } else {
-            // 右子树是满二叉树
-            return (1 << rightDepth) + countNodes(root->left);
-        }
+        if (root == NULL) return 0;
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
