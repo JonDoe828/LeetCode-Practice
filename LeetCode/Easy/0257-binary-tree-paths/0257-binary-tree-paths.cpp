5436346ea3960601c80abcc1cd6f658be6b5b1a9
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-01-28 11:04:14 +1100

    Time: 0 ms (100%), Space: 17.2 MB (87.72%) - LeetHub

--- LeetCode/Easy/0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ LeetCode/Easy/0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -11,24 +11,32 @@
  * };
  */
 class Solution {
-    void traversal(TreeNode* cur, string path, vector<string>& result) {
-        path += to_string(cur->val); // Process middle node
-        if (cur->left == NULL && cur->right == NULL) {
-            result.push_back(path);
+    void traversal(TreeNode* cur, vector<int>& path, vector<string>& result) {
+        path.push_back(cur->val); // Process middle node, add to path
+        if (cur->left == NULL && cur->right == NULL) { // Found a leaf node
+            string sPath;
+            for (int i = 0; i < path.size() - 1; i++) {
+                sPath += to_string(path[i]);
+                sPath += "->";
+            }
+            sPath += to_string(path[path.size() - 1]);
+            result.push_back(sPath); // Collect a path
             return;
         }
-        if (cur->left)
-            traversal(cur->left, path + "->", result); // Process left
-        if (cur->right)
-            traversal(cur->right, path + "->", result); // Process right
+        if (cur->left) { // Process left
+            traversal(cur->left, path, result);
+            path.pop_back(); // Backtrack
+        }
+        if (cur->right) { // Process right
+            traversal(cur->right, path, result);
+            path.pop_back(); // Backtrack
+        }
     }
-
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
-        vector<string> result;
-        string path;
-        if (root == NULL)
-            return result;
+vector<string> result;
+        vector<int> path;
+        if (root == NULL) return result;
         traversal(root, path, result);
         return result;
     }
