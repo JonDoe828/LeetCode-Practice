f9a6895d99d1d3b19e3332fc81a69602801ef8ca
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-02-10 17:50:47 +1100

    Time: 0 ms (100%), Space: 20.2 MB (22.86%) - LeetHub

--- LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,14 +1,27 @@
+//DP
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int len = prices.size();
+//         vector<vector<int>> dp(len, vector<int>(2, 0));
+//         dp[0][0] -= prices[0];
+//         dp[0][1] = 0;
+//         for (int i = 1; i < len; i++) {
+//             dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+//             dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+//         }
+//         return dp[len - 1][1];
+//     }
+// };
+
+//Greedy
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int len = prices.size();
-        vector<vector<int>> dp(len, vector<int>(2, 0));
-        dp[0][0] -= prices[0];
-        dp[0][1] = 0;
-        for (int i = 1; i < len; i++) {
-            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
-            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        int result = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            result += max(prices[i] - prices[i - 1], 0);
         }
-        return dp[len - 1][1];
+        return result;
     }
 };
