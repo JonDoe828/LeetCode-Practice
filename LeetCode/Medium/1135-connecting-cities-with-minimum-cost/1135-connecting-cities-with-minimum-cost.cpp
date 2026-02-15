bd460ef49d498d26dbc25bf4757b6d0b3c642691
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-02-15 19:11:02 +1100

    leetcode: 1135-connecting-cities-with-minimum-cost [Medium] (.cpp) | 02-15-2026 | Time:32 ms (51.46%) Space:46.9 MB (84.52%)

--- LeetCode/Medium/1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.cpp
+++ LeetCode/Medium/1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.cpp
@@ -47,86 +47,87 @@
 //     }
 // };
 
-class Solution {
-public:
-    int minimumCost(int n, vector<vector<int>>& connections) {
-        using P = pair<int,int>; // (weight, node)
-
-        vector<vector<pair<int,int>>> adj(n + 1);
-        for (auto &e : connections) {
-            int u = e[0], v = e[1], w = e[2];
-            adj[u].push_back({v, w});
-            adj[v].push_back({u, w});
-        }
-
-        vector<bool> inMST(n + 1, false);
-        priority_queue<P, vector<P>, greater<P>> pq;
-
-        // 从1开始
-        pq.push({0, 1});
-
-        long long cost = 0;
-        int picked = 0;
-
-        while (!pq.empty() && picked < n) {
-            auto [w, u] = pq.top();
-            pq.pop();
-
-            if (inMST[u]) continue;
-
-            inMST[u] = true;
-            cost += w;
-            picked++;
-
-            for (auto [v, w2] : adj[u]) {
-                if (!inMST[v]) pq.push({w2, v});
-            }
-        }
-
-        return picked == n ? (int)cost : -1;
-    }
-};
-
-
-// Kruskal
+// Prim+邻接表+堆优化
 // class Solution {
 // public:
 //     int minimumCost(int n, vector<vector<int>>& connections) {
-//         sort(connections.begin(), connections.end(),
-//              [](const auto& a, const auto& b) { return a[2] < b[2]; });
+//         using P = pair<int,int>; // (weight, node)
 
-//         vector<int> parent(n + 1), rankv(n + 1, 0);
-//         for (int i = 1; i <= n; ++i)
-//             parent[i] = i;
+//         vector<vector<pair<int,int>>> adj(n + 1);
+//         for (auto &e : connections) {
+//             int u = e[0], v = e[1], w = e[2];
+//             adj[u].push_back({v, w});
+//             adj[v].push_back({u, w});
+//         }
 
-//         auto find = [&](auto&& self, int x) -> int {
-//             if (parent[x] != x)
-//                 parent[x] = self(self, parent[x]);
-//             return parent[x];
-//         };
+//         vector<bool> inMST(n + 1, false);
+//         priority_queue<P, vector<P>, greater<P>> pq;
 
-//         auto unite = [&](int a, int b) -> bool {
-//             int pa = find(find, a), pb = find(find, b);
-//             if (pa == pb)
-//                 return false;
-//             if (rankv[pa] < rankv[pb])
-//                 swap(pa, pb);
-//             parent[pb] = pa;
-//             if (rankv[pa] == rankv[pb])
-//                 rankv[pa]++;
-//             return true;
-//         };
+//         // 从1开始
+//         pq.push({0, 1});
 
 //         long long cost = 0;
-//         int used = 0;
-//         for (auto& e : connections) {
-//             if (unite(e[0], e[1])) {
-//                 cost += e[2];
-//                 used++;
-//                 if (used == n - 1)
-//                     break;
+//         int picked = 0;
+
+//         while (!pq.empty() && picked < n) {
+//             auto [w, u] = pq.top();
+//             pq.pop();
+
+//             if (inMST[u]) continue;
+
+//             inMST[u] = true;
+//             cost += w;
+//             picked++;
+
+//             for (auto [v, w2] : adj[u]) {
+//                 if (!inMST[v]) pq.push({w2, v});
 //             }
 //         }
-//         return used == n - 1 ? (int)cost : -1;
+
+//         return picked == n ? (int)cost : -1;
 //     }
 // };
+
+
+// Kruskal
+class Solution {
+public:
+    int minimumCost(int n, vector<vector<int>>& connections) {
+        sort(connections.begin(), connections.end(),
+             [](const auto& a, const auto& b) { return a[2] < b[2]; });
+
+        vector<int> parent(n + 1), rankv(n + 1, 0);
+        for (int i = 1; i <= n; ++i)
+            parent[i] = i;
+
+        auto find = [&](auto&& self, int x) -> int {
+            if (parent[x] != x)
+                parent[x] = self(self, parent[x]);
+            return parent[x];
+        };
+
+        auto unite = [&](int a, int b) -> bool {
+            int pa = find(find, a), pb = find(find, b);
+            if (pa == pb)
+                return false;
+            if (rankv[pa] < rankv[pb])
+                swap(pa, pb);
+            parent[pb] = pa;
+            if (rankv[pa] == rankv[pb])
+                rankv[pa]++;
+            return true;
+        };
+
+        long long cost = 0;
+        int used = 0;
+        for (auto& e : connections) {
+            if (unite(e[0], e[1])) {
+                cost += e[2];
+                used++;
+                if (used == n - 1)
+                    break;
+            }
+        }
+        return used == n - 1 ? (int)cost : -1;
+    }
+};
