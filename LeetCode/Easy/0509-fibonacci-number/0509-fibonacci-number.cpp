efe1e071c4f5ed7e0d7d990bf34f9a3bf54d972a
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-02-15 19:04:00 +1100

    leetcode: 0509-fibonacci-number [Easy] (.cpp) | 02-15-2026 | Time:3 ms (65.16%) Space:7.6 MB (98.27%)

---
+++ LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int fib(int n) {
+        // if (n <= 1) {
+        //     return n;
+        // }
+
+        int a = 0, b = 1;
+        // for (int i = 2; i <= n; i++) {
+        //     int s = a + b;
+        //     a = b;
+        //     b = s;
+        // }
+        // return b;
+
+        for (int i = 0; i < n; i++) {
+            tie(a, b) = make_tuple(b, a + b);
+        }
+        return a;
+    }
+};
