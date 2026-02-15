78bb9cf67620b0f9fca7904940bdcec05e0cb929
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-02-15 19:12:09 +1100

    leetcode: 1135-connecting-cities-with-minimum-cost [Medium] (.cpp) | 02-15-2026 | Time:8 ms (95.4%) Space:47.2 MB (64.85%)

--- LeetCode/Medium/1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.cpp
+++ LeetCode/Medium/1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.cpp
@@ -1,3 +1,46 @@
+// Kruskal
+class Solution {
+public:
+    int minimumCost(int n, vector<vector<int>>& connections) {
+        sort(connections.begin(), connections.end(),
+             [](const auto& a, const auto& b) { return a[2] < b[2]; });
+
+        vector<int> parent(n + 1), rankv(n + 1, 0);
+        for (int i = 1; i <= n; ++i)
+            parent[i] = i;
+
+        auto find = [&](auto&& self, int x) -> int {
+            if (parent[x] != x)
+                parent[x] = self(self, parent[x]);
+            return parent[x];
+        };
+
+        auto unite = [&](int a, int b) -> bool {
+            int pa = find(find, a), pb = find(find, b);
+            if (pa == pb)
+                return false;
+            if (rankv[pa] < rankv[pb])
+                swap(pa, pb);
+            parent[pb] = pa;
+            if (rankv[pa] == rankv[pb])
+                rankv[pa]++;
+            return true;
+        };
+
+        long long cost = 0;
+        int used = 0;
+        for (auto& e : connections) {
+            if (unite(e[0], e[1])) {
+                cost += e[2];
+                used++;
+                if (used == n - 1)
+                    break;
+            }
+        }
+        return used == n - 1 ? (int)cost : -1;
+    }
+};
+
 // Prim
 // class Solution {
 // public:
@@ -89,45 +132,3 @@
 // };
 
 
-// Kruskal
-class Solution {
-public:
-    int minimumCost(int n, vector<vector<int>>& connections) {
-        sort(connections.begin(), connections.end(),
-             [](const auto& a, const auto& b) { return a[2] < b[2]; });
-
-        vector<int> parent(n + 1), rankv(n + 1, 0);
-        for (int i = 1; i <= n; ++i)
-            parent[i] = i;
-
-        auto find = [&](auto&& self, int x) -> int {
-            if (parent[x] != x)
-                parent[x] = self(self, parent[x]);
-            return parent[x];
-        };
-
-        auto unite = [&](int a, int b) -> bool {
-            int pa = find(find, a), pb = find(find, b);
-            if (pa == pb)
-                return false;
-            if (rankv[pa] < rankv[pb])
-                swap(pa, pb);
-            parent[pb] = pa;
-            if (rankv[pa] == rankv[pb])
-                rankv[pa]++;
-            return true;
-        };
-
-        long long cost = 0;
-        int used = 0;
-        for (auto& e : connections) {
-            if (unite(e[0], e[1])) {
-                cost += e[2];
-                used++;
-                if (used == n - 1)
-                    break;
-            }
-        }
-        return used == n - 1 ? (int)cost : -1;
-    }
-};
