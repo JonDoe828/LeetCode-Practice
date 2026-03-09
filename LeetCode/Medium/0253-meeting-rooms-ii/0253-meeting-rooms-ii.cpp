5d4b143f30a0df470a008dd4a0b9fdbb0333d38f
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-03-09 21:36:34 +1100

    leetcode: 0253-meeting-rooms-ii [Medium] (.cpp) | 03-09-2026 | Time:3 ms (56.82%) Space:16.4 MB (51.51%)

--- LeetCode/Medium/0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ LeetCode/Medium/0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,22 +1,58 @@
+// class Solution {
+// public:
+//     int minMeetingRooms(vector<vector<int>>& intervals) {
+//         if (intervals.empty())
+//             return 0;
+
+//         sort(intervals.begin(), intervals.end());
+
+//         priority_queue<int, vector<int>, greater<int>> pq;
+
+//         pq.push(intervals[0][1]);
+
+//         for (int i = 1; i < intervals.size(); i++) {
+//             if (intervals[i][0] >= pq.top()) {
+//                 pq.pop();
+//             }
+//             pq.push(intervals[i][1]);
+//         }
+
+//         return pq.size();
+//     }
+// };
+
+
+//扫描线
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        if (intervals.empty())
-            return 0;
 
-        sort(intervals.begin(), intervals.end());
+        int n = intervals.size();
 
-        priority_queue<int, vector<int>, greater<int>> pq;
+        vector<int> start(n), end(n);
 
-        pq.push(intervals[0][1]);
-
-        for (int i = 1; i < intervals.size(); i++) {
-            if (intervals[i][0] >= pq.top()) {
-                pq.pop();
-            }
-            pq.push(intervals[i][1]);
+        for (int i = 0; i < n; i++) {
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
         }
 
-        return pq.size();
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
+
+        int rooms = 0, maxRooms = 0;
+        int i = 0, j = 0;
+
+        while (i < n) {
+            if (start[i] < end[j]) {
+                rooms++;
+                i++;
+            } else {
+                rooms--;
+                j++;
+            }
+            maxRooms = max(maxRooms, rooms);
+        }
+
+        return maxRooms;
     }
 };
