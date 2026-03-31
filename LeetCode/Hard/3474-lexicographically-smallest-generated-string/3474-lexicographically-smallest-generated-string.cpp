572f17b819a8d3b05daae65f847ad306525d755e
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-04-01 00:23:12 +1100

    leetcode: 3474-lexicographically-smallest-generated-string [Hard] (.cpp) | 04-01-2026 | Time:15 ms (79.66%) Space:11.1 MB (77.97%)

--- LeetCode/Hard/3474-lexicographically-smallest-generated-string/3474-lexicographically-smallest-generated-string.cpp
+++ LeetCode/Hard/3474-lexicographically-smallest-generated-string/3474-lexicographically-smallest-generated-string.cpp
@@ -1,98 +1,64 @@
 class Solution {
 public:
-    string str1, str2;
-    int n, m, L;
-    vector<int> pi;
-    vector<vector<int>> go;
-    vector<vector<int>> memo;
+    string generateString(string str1, string str2) {
+        int n = str1.size();
+        int m = str2.size();
+        int len = n + m - 1;
 
-    vector<int> buildPi(const string& p) {
-        int m = p.size();
-        vector<int> pi(m, 0);
-        for (int i = 1; i < m; i++) {
-            int j = pi[i - 1];
-            while (j > 0 && p[i] != p[j]) j = pi[j - 1];
-            if (p[i] == p[j]) j++;
-            pi[i] = j;
-        }
-        return pi;
-    }
+        string ans(len, '?');
+        vector<bool> fixed(len, false);
 
-    vector<vector<int>> buildGo(const string& p, const vector<int>& pi) {
-        int m = p.size();
-        vector<vector<int>> go(m + 1, vector<int>(26, 0));
-
-        for (int state = 0; state <= m; state++) {
-            for (int c = 0; c < 26; c++) {
-                char ch = 'a' + c;
-                if (state < m && ch == p[state]) {
-                    go[state][c] = state + 1;
-                } else {
-                    if (state == 0) go[state][c] = 0;
-                    else go[state][c] = go[pi[state - 1]][c];
+        // 1. 先处理 T
+        for (int i = 0; i < n; i++) {
+            if (str1[i] == 'T') {
+                for (int j = 0; j < m; j++) {
+                    int pos = i + j;
+                    if (ans[pos] == '?') {
+                        ans[pos] = str2[j];
+                        fixed[pos] = true;
+                    } else if (ans[pos] != str2[j]) {
+                        return "";
+                    } else {
+                        fixed[pos] = true;
+                    }
                 }
             }
         }
-        return go;
-    }
 
-    bool validTransition(int pos, int j, int c, int& nextState) {
-        int raw = go[j][c];
-        int start = pos - m + 1;
-
-        if (start >= 0) {
-            bool matched = (raw == m);
-            if (str1[start] == 'T' && !matched) return false;
-            if (str1[start] == 'F' && matched) return false;
+        // 2. 其余位置先填 a
+        for (int i = 0; i < len; i++) {
+            if (ans[i] == '?') ans[i] = 'a';
         }
 
-        if (raw == m) nextState = pi[m - 1];
-        else nextState = raw;
-
-        return true;
-    }
-
-    bool dfs(int pos, int j) {
-        if (pos == L) return true;
-        if (memo[pos][j] != -1) return memo[pos][j];
-
-        for (int c = 0; c < 26; c++) {
-            int nj;
-            if (!validTransition(pos, j, c, nj)) continue;
-            if (dfs(pos + 1, nj)) {
-                return memo[pos][j] = 1;
-            }
-        }
-        return memo[pos][j] = 0;
-    }
-
-    string generateString(string s1, string s2) {
-        str1 = s1;
-        str2 = s2;
-        n = str1.size();
-        m = str2.size();
-        L = n + m - 1;
-
-        pi = buildPi(str2);
-        go = buildGo(str2, pi);
-        memo.assign(L + 1, vector<int>(m + 1, -1));
-
-        if (!dfs(0, 0)) return "";
-
-        string ans;
-        int pos = 0, j = 0;
-        while (pos < L) {
-            for (int c = 0; c < 26; c++) {
-                int nj;
-                if (!validTransition(pos, j, c, nj)) continue;
-                if (dfs(pos + 1, nj)) {
-                    ans.push_back('a' + c);
-                    j = nj;
-                    break;
+        // 3. 处理 F
+        for (int i = 0; i < n; i++) {
+            if (str1[i] == 'F') {
+                bool equal = true;
+                for (int j = 0; j < m; j++) {
+                    if (ans[i + j] != str2[j]) {
+                        equal = false;
+                        break;
+                    }
                 }
+
+                if (!equal) continue;
+
+                // 当前窗口刚好等于 str2，必须改一个位置
+                bool changed = false;
+                for (int j = m - 1; j >= 0; j--) {
+                    int pos = i + j;
+                    if (!fixed[pos]) {
+                        // 改成最小的不一样字符
+                        ans[pos] = (str2[j] == 'a') ? 'b' : 'a';
+                        changed = true;
+                        break;
+                    }
+                }
+
+                if (!changed) return "";
             }
-            pos++;
         }
+
         return ans;
     }
 };
