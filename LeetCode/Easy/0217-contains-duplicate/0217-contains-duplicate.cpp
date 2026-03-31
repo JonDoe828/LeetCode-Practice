e9adcfb91be0b64ed2b6d6a39ed98a8d6768267c
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-03-31 23:04:03 +1100

    leetcode: 0217-contains-duplicate [Easy] (.cpp) | 03-31-2026 | Time:20 ms (91.68%) Space:71 MB (88.58%)

--- LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.cpp
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,25 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         unordered_set<int> set;
+//         set.reserve(nums.size());
+//         for (int i : nums) {
+
+//             if (!set.insert(i).second) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> set;
-        set.reserve(nums.size());
-        for (int i : nums) {
-
-            if (!set.insert(i).second) {
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) {
                 return true;
             }
         }
