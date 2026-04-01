bb32def3ab3198d358b0cdf7addad19ee31ba101
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-04-02 02:31:56 +1100

    leetcode: 0392-is-subsequence [Easy] (.cpp) | 04-02-2026 | Time:0 ms (100%) Space:8.4 MB (99.61%)

--- LeetCode/Easy/0392-is-subsequence/0392-is-subsequence.cpp
+++ LeetCode/Easy/0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,31 +1,13 @@
-// class Solution {
-// public:
-//     bool isSubsequence(string s, string t) {
-//         int m = s.size(), n = t.size();
-//         vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-//         for (int i = 1; i <= m; ++i) {
-//             for (int j = 1; j <= n; ++j) {
-//                 if (s[i - 1] == t[j - 1]) {
-//                     dp[i][j] = dp[i - 1][j - 1] + 1;
-//                 } else {
-//                     dp[i][j] = dp[i][j - 1];
-//                 }
-//             }
-//         }
-//         return dp[m][n] == m;
-//     }
-// };
-
-
-//双指针
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
         int i = 0, j = 0;
-        while (i < (int)s.size() && j < (int)t.size()) {
-            if (s[i] == t[j]) ++i;
-            ++j;
+        while (i < s.size() && j < t.size()) {
+            if (s[i] == t[j]) {
+                i++;
+            }
+            j++;
         }
-        return i == (int)s.size();
+        return i == s.size();
     }
 };
