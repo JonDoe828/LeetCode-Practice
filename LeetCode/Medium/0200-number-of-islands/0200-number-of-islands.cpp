b99056bd127ed7b24e20c5736649eac037af50b4
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-04-02 00:55:42 +1100

    leetcode: 0200-number-of-islands [Medium] (.cpp) | 04-02-2026 | Time:29 ms (42.12%) Space:16.2 MB (78.03%)

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.cpp
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
     int count = 0;
-
-    void bfs(vector<vector<char>>& grid, int x, int y) {
-        queue<pair<int, int>> que;
-        que.push({x, y});
-        grid[x][y] = '0';
-
-        int dir[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-        while (!que.empty()) {
-            int curX = que.front().first;
-            int curY = que.front().second;
-            que.pop();
-
-            for (int i = 0; i < 4; i++) {
-                int nextX = curX + dir[i][0];
-                int nextY = curY + dir[i][1];
-
-                if (nextX < 0 || nextX >= grid.size() || nextY < 0 ||
-                    nextY >= grid[0].size()) {
-                    continue;
-                }
-
-                if (grid[nextX][nextY] == '1') {
-                    que.push({nextX, nextY});
-                    grid[nextX][nextY] = '0'; 
-                }
-            }
+    void dfs(vector<vector<char>>& grid, int x, int y) {
+        if (x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size()) {
+            return;
         }
+        if (grid[x][y] == '0') {
+            return;
+        }
+
+        grid[x][y] = '0';
+        dfs(grid, x + 1, y);
+        dfs(grid, x, y + 1);
+        dfs(grid, x - 1, y);
+        dfs(grid, x, y - 1);
     }
 
     int numIslands(vector<vector<char>>& grid) {
@@ -36,11 +21,58 @@ public:
             for (int j = 0; j < grid[0].size(); j++) {
                 if (grid[i][j] == '1') {
                     count++;
-                    bfs(grid, i, j);
+                    dfs(grid, i, j);
                 }
             }
         }
-
         return count;
     }
 };
+
+// bfs
+//  class Solution {
+//  public:
+//      int count = 0;
+
+//     void bfs(vector<vector<char>>& grid, int x, int y) {
+//         queue<pair<int, int>> que;
+//         que.push({x, y});
+//         grid[x][y] = '0';
+
+//         int dir[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+//         while (!que.empty()) {
+//             int curX = que.front().first;
+//             int curY = que.front().second;
+//             que.pop();
+
+//             for (int i = 0; i < 4; i++) {
+//                 int nextX = curX + dir[i][0];
+//                 int nextY = curY + dir[i][1];
+
+//                 if (nextX < 0 || nextX >= grid.size() || nextY < 0 ||
+//                     nextY >= grid[0].size()) {
+//                     continue;
+//                 }
+
+//                 if (grid[nextX][nextY] == '1') {
+//                     que.push({nextX, nextY});
+//                     grid[nextX][nextY] = '0';
+//                 }
+//             }
+//         }
+//     }
+
+//     int numIslands(vector<vector<char>>& grid) {
+//         for (int i = 0; i < grid.size(); i++) {
+//             for (int j = 0; j < grid[0].size(); j++) {
+//                 if (grid[i][j] == '1') {
+//                     count++;
+//                     bfs(grid, i, j);
+//                 }
+//             }
+//         }
+
+//         return count;
+//     }
+// };
