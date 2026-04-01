c591217777834dd9507cdccbef6db49db0ac7567
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-04-02 02:24:43 +1100

    leetcode: 1768-merge-strings-alternately [Easy] (.cpp) | 04-02-2026 | Time:4 ms (31.55%) Space:8.6 MB (32.72%)

---
+++ LeetCode/Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res;
+        int i = 0, j = 0;
+        int n1 = word1.size(), n2 = word2.size();
+
+        while (i < n1 || j < n2) {
+            if (i < n1) {
+                res+= word1[i++];
+            }
+            if (j < n2) {
+                res.push_back(word2[j]);
+                j++;
+            }
+        }
+        return res;
+    }
+};
