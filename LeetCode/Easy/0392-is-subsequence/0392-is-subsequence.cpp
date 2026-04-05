726234d63dee0b28b00fdce031dde5f9a82e9e4c
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-04-05 10:20:04 +1000

    leetcode: 0392-is-subsequence [Easy] (.cpp) | 04-05-2026 | Time:0 ms (100%) Space:8.4 MB (99.58%)

--- LeetCode/Easy/0392-is-subsequence/0392-is-subsequence.cpp
+++ LeetCode/Easy/0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int i = 0, j = 0;
-        while (i < s.size() && j < t.size()) {
+        int n = s.size(), m = t.size(), i = 0, j = 0;
+        while (i < n && j < m) {
             if (s[i] == t[j]) {
                 i++;
             }
