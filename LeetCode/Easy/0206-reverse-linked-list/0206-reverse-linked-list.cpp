32ab1b181502190be06ef6c48fca03bde94591bc
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-04-05 21:18:17 +1000

    leetcode: 0206-reverse-linked-list [Easy] (.cpp) | 04-05-2026 | Time:0 ms (100%) Space:13.4 MB (41.4%)

--- LeetCode/Easy/0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ LeetCode/Easy/0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -8,32 +8,17 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
- //Two-Pointer Method
-// class Solution {
-// public:
-//     ListNode* reverseList(ListNode* head) {
-//         ListNode* prev = nullptr;
-//         ListNode* cur = head;
-//         while (cur) {
-//             ListNode* temp = cur->next; // 1. 保存
-//             cur->next = prev;           // 2. 反转
-//             prev = cur;                 // 3. 移动
-//             cur = temp;
-//         }
-//         return prev;
-//     }
-// };
-
-
-//Recursive
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if (!head || !head->next) return head;
-        ListNode* newHead = reverseList(head->next);
-        head->next->next = head;
-        head->next = nullptr;
-        return newHead;
+        ListNode* prev = nullptr;
+        ListNode* cur = head;
+        while (cur != nullptr) {
+            ListNode* temp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = temp;
+        }
+        return prev;
     }
 };
