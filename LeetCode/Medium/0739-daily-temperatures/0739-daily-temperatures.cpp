f00778d6796072837b8c6dfcd7a2252a4d533c0f
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-04-05 10:35:01 +1000

    leetcode: 0739-daily-temperatures [Medium] (.cpp) | 04-05-2026 | Time:28 ms (30.33%) Space:107.4 MB (38.61%)

--- LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.cpp
+++ LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,41 +1,17 @@
-// class Solution {
-// public:
-//     vector<int> dailyTemperatures(vector<int>& temperatures) {
-//         int n = temperatures.size();
-//         stack<int> st;
-//         vector<int> res(n, 0);
-//         st.push(0);
-
-//         for (int i = 1; i < n; i++) {
-//             if (temperatures[i] < temperatures[st.top()]) {
-//                 st.push(i);
-//             } else if (temperatures[i] == temperatures[st.top()]) {
-//                 st.push(i);
-//             } else {
-//                 while (!st.empty() &&
-//                        temperatures[i] > temperatures[st.top()]) {
-//                     res[st.top()] = i - st.top();
-//                     st.pop();
-//                 }
-//                 st.push(i);
-//             }
-//         }
-//         return res;
-//     }
-// };
-
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& T) {
-        stack<int> st; // 递增栈
-        vector<int> result(T.size(), 0);
-        for (int i = 0; i < T.size(); i++) {
-            while (!st.empty() && T[i] > T[st.top()]) { // 注意栈不能为空
-                result[st.top()] = i - st.top();
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> st;
+        vector<int> res(temperatures.size(), 0);
+
+        for (int i = 0; i < temperatures.size(); i++) {
+            while (!st.empty() && temperatures[st.top()] < temperatures[i]) {
+                res[st.top()] = i - st.top();
                 st.pop();
             }
+
             st.push(i);
         }
-        return result;
+        return res;
     }
 };
