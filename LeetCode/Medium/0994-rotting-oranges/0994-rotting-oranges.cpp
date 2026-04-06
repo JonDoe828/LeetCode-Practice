fbf900b5e508c182321f501c936f0637707e86b0
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-04-06 22:51:01 +1000

    leetcode: 0994-rotting-oranges [Medium] (.cpp) | 04-06-2026 | Time:0 ms (100%) Space:16.7 MB (86.45%)

--- LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.cpp
+++ LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -6,17 +6,18 @@ public:
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 2)
-                    q.push({i, j});
-                if (grid[i][j] == 1)
+                if (grid[i][j] == 1) {
                     fresh++;
+                } else if (grid[i][j] == 2) {
+                    q.push({i, j});
+                }
             }
         }
 
         if (fresh == 0)
             return 0;
 
-        int dir[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int dir[4][2] = {{1, 0}, {0, 1}, {0, -1}, {-1, 0}};
 
         while (!q.empty()) {
             int size = q.size();
@@ -30,8 +31,10 @@ public:
                     int nx = x + dir[k][0];
                     int ny = y + dir[k][1];
 
-                    if (nx >= 0 && nx < m && ny >= 0 && ny < n &&
-                        grid[nx][ny] == 1) {
+                    if (nx < 0 || ny < 0 || nx >= m || ny >= n) {
+                        continue;
+                    }
+                    if (grid[nx][ny] == 1) {
                         grid[nx][ny] = 2;
                         fresh--;
                         q.push({nx, ny});
