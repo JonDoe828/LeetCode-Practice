396e0285340ee1e3b59670044eab5783caedb6e2
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-04-06 16:27:48 +1000

    leetcode: 0994-rotting-oranges [Medium] (.cpp) | 04-06-2026 | Time:0 ms (100%) Space:16.8 MB (70.4%)

---
+++ LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        queue<pair<int, int>> q;
+        int fresh = 0, minutes = 0, m = grid.size(), n = grid[0].size();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2)
+                    q.push({i, j});
+                if (grid[i][j] == 1)
+                    fresh++;
+            }
+        }
+
+        if (fresh == 0)
+            return 0;
+
+        int dir[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        while (!q.empty()) {
+            int size = q.size();
+            bool changed = false;
+
+            for (int i = 0; i < size; i++) {
+                auto [x, y] = q.front();
+                q.pop();
+
+                for (int k = 0; k < 4; k++) {
+                    int nx = x + dir[k][0];
+                    int ny = y + dir[k][1];
+
+                    if (nx >= 0 && nx < m && ny >= 0 && ny < n &&
+                        grid[nx][ny] == 1) {
+                        grid[nx][ny] = 2;
+                        fresh--;
+                        q.push({nx, ny});
+                        changed = true;
+                    }
+                }
+            }
+            if (changed)
+                minutes++;
+        }
+
+        return fresh == 0 ? minutes : -1;
+    }
+};
