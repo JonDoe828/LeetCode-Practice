ff08ab739ee51505b1f4b147210713e10053ec37
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-04-09 21:28:43 +1000

    leetcode: 0005-longest-palindromic-substring [Medium] (.cpp) | 04-09-2026 | Time:4 ms (94.42%) Space:9.4 MB (65.4%)

---
+++ LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    string longestPalindrome(string s) {
+        int start = 0;
+        int maxLen = 1;
+
+        for (int i = 0; i < s.size(); i++) {
+            int left = i;
+            int right = i;
+            while (left >= 0 && right < s.size() && s[left] == s[right]) {
+                left--;
+                right++;
+            }
+            int len = right - left - 1;
+
+            if (len > maxLen) {
+                maxLen = len;
+                start = left + 1;
+            }
+        }
+
+        for (int i = 0; i < s.size(); i++) {
+            int left = i;
+            int right = i + 1;
+            while (left >= 0 && right < s.size() && s[left] == s[right]) {
+                left--;
+                right++;
+            }
+            int len = right - left - 1;
+
+            if (len > maxLen) {
+                maxLen = len;
+                start = left + 1;
+            }
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
