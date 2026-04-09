4bec5bda63f94213358ea2a383bcac35d938ee3f
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-04-09 12:47:12 +1000

    leetcode: 0215-kth-largest-element-in-an-array [Medium] (.cpp) | 04-09-2026 | Time:35 ms (46.5%) Space:61.7 MB (34%)

--- LeetCode/Medium/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ LeetCode/Medium/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,37 +1,16 @@
 class Solution {
 public:
-    int partition(vector<int>& nums, int left, int right) {
-        int randomIndex = left + rand() % (right - left + 1);
-        swap(nums[randomIndex], nums[right]);
-
-        int pivot = nums[right];
-        int i = left;
-
-        for (int j = left; j < right; j++) {
-            if (nums[j] < pivot) {
-                swap(nums[i], nums[j]);
-                i++;
-            }
-        }
-
-        swap(nums[i], nums[right]);
-        return i;
-    }
 
     int findKthLargest(vector<int>& nums, int k) {
-        int target = nums.size() - k;
-        int left = 0, right = nums.size() - 1;
+        priority_queue<int, vector<int>, greater<int>> pq;
 
-        while (right >= left) {
-            int p = partition(nums, left, right);
-            if (p == target) {
-                return nums[p];
-            } else if (p < target) {
-                left = p + 1;
-            } else {
-                right = p - 1;
+        for (int x : nums) {
+            pq.push(x);
+            if (pq.size() > k) {
+                pq.pop();
             }
         }
-        return -1;
+
+        return pq.top();
     }
 };
