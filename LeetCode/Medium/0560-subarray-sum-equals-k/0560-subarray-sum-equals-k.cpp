650a54f930220f5c2dae754e5f216100534de6e4
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-04-09 12:17:12 +1000

    leetcode: 0560-subarray-sum-equals-k [Medium] (.cpp) | 04-09-2026 | Time:34 ms (88.46%) Space:45.4 MB (69.76%)

--- LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,20 +3,15 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int, int> mp;
         mp[0] = 1;
-
-        int sum = 0;
-        int count = 0;
+        int count = 0, sum = 0;
 
         for (int x : nums) {
             sum += x;
-
             if (mp.count(sum - k)) {
                 count += mp[sum - k];
             }
-
             mp[sum]++;
         }
-
         return count;
     }
 };
