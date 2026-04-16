e8d303d9ba44f5948654b8eef2f349c8533450d5
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-04-16 19:08:00 +1000

    leetcode: 0011-container-with-most-water [Medium] (.cpp) | 04-16-2026 | Time:0 ms (100%) Space:62.9 MB (78.14%)

---
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int maxWater = 0;
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int water = min(height[left], height[right]) * (right - left);
+            maxWater = max(maxWater, water);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxWater;
+    }
+};
