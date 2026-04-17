4c256603e2ab0ba31df5c9b44d3833aa51e2e9c5
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-04-17 20:19:34 +1000

    leetcode: 0268-missing-number [Easy] (.cpp) | 04-17-2026 | Time:0 ms (100%) Space:21.9 MB (37.56%)

--- LeetCode/Easy/0268-missing-number/0268-missing-number.cpp
+++ LeetCode/Easy/0268-missing-number/0268-missing-number.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int sum = 0, sum1 = 0;
+        int sum = 0;
         for (int x : nums) {
             sum += x;
         }
-        for (int i = 0; i <= n; i++) {
-            sum1 += i;
-        }
+        int sum1 = n * (n + 1) / 2;
         return sum1 - sum;
     }
 };
