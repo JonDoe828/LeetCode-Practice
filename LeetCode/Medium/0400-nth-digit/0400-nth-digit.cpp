de36f041755def75b7cce1f5197c56ce3f791f0c
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-04-22 05:27:26 +1000

    leetcode: 0400-nth-digit [Medium] (.cpp) | 04-22-2026 | Time:0 ms (100%) Space:7.8 MB (95.01%)

--- LeetCode/Medium/0400-nth-digit/0400-nth-digit.cpp
+++ LeetCode/Medium/0400-nth-digit/0400-nth-digit.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int findNthDigit(int n) {
-        long long digit = 1; // 当前这一组数字的位数：1位数、2位数、3位数...
+        long long weight  = 1; // 当前这一组数字的位数：1位数、2位数、3位数...
         long long start = 1; // 当前这一组的起始数字：1、10、100...
-        long long count = 9; // 当前这一组一共有多少位：9*1, 90*2, 900*3...
+        long long totalWeight = 9; // 当前这一组一共有多少位：9*1, 90*2, 900*3...
 
         // 先确定 n 落在哪一组
-        while (n > count) {
-            n -= count;
-            digit++;
+        while (n > totalWeight) {
+            n -= totalWeight;
+            weight++;
             start *= 10;
-            count = 9 * start * digit;
+            totalWeight = 9 * start * weight;
         }
 
         // 找到具体是这一组里的哪个数
-        long long num = start + (n - 1) / digit;
+        long long num = start + (n - 1) / weight;
 
         // 找到是这个数里的第几位
-        int index = (n - 1) % digit;
+        int index = (n - 1) % weight;
 
         string s = to_string(num);
         return s[index] - '0';
