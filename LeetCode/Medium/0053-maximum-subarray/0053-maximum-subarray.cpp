504097dc12b55567a4bf302732cad8b4efb72b70
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-04-21 19:05:12 +1000

    leetcode: 0053-maximum-subarray [Medium] (.cpp) | 04-21-2026 | Time:2 ms (21.78%) Space:74.5 MB (5.01%)

--- LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.cpp
+++ LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,55 +1,14 @@
 class Solution {
 public:
-    // int maxSubArray(vector<int>& nums) {
-    //     if (nums.empty()) {
-    //         return 0;
-    //     }
-    //     int ans = INT_MIN;
-    //     int curPrefixsum = 0;
-    //     int minPrefixSum = 0;
-    //     for (int num : nums) {
-    //         curPrefixsum += num;
-    //         ans = max(ans, curPrefixsum - minPrefixSum);
-    //         minPrefixSum = min(minPrefixSum, curPrefixsum);
-    //     }
-    //     return ans;
-    // }
-
-    //     int maxSubArray(vector<int>& nums) {
-    //     int n = (int)nums.size();
-    //     vector<int> dp(n);
-    //     dp[0] = nums[0];
-    //     int ans = dp[0];
-    //     for (int i = 1; i < n; ++i) {
-    //         dp[i] = max(nums[i], dp[i - 1] + nums[i]);
-    //         ans = max(ans, dp[i]);
-    //     }
-    //     return ans;
-    // }
-
-    // DP
-    //  int maxSubArray(vector<int>& nums) {
-    //      int n = nums.size();
-    //      if (n == 0)
-    //          return 0;
-    //      vector<int> dp(n);
-    //      dp[0] = nums[0];
-    //      int res = dp[0];
-
-    //     for (int i = 1; i < n; i++) {
-    //         dp[i] = max(nums[i], dp[i - 1] + nums[i]);
-    //         res = max(res, dp[i]);
-    //     }
-    //     return res;
-    // }
-
-    // Kadane(Greedy with DP justification)
     int maxSubArray(vector<int>& nums) {
-        int cur = nums[0], res = nums[0];
-        for (int i = 1; i < (int)nums.size(); ++i) {
-            cur = max(nums[i], cur + nums[i]);
-            res = max(res, cur);
+        vector<int> dp(nums.size() + 1, 0);
+        dp[0] = nums[0];
+        int ans = dp[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+            ans = max(ans, dp[i]);
         }
-        return res;
+        return ans;
     }
 };
