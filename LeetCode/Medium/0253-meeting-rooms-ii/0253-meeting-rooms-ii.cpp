d08d0acfdb293b390187696ac811bc4e445f6f2b
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-04-24 23:09:27 +1000

    leetcode: 0253-meeting-rooms-ii [Medium] (.cpp) | 04-24-2026 | Time:4 ms (34.45%) Space:16.8 MB (28.26%)

--- LeetCode/Medium/0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ LeetCode/Medium/0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,83 +1,29 @@
-//小顶堆
-// class Solution {
-// public:
-//     int minMeetingRooms(vector<vector<int>>& intervals) {
-//         if (intervals.empty())
-//             return 0;
-
-//         sort(intervals.begin(), intervals.end());
-
-//         priority_queue<int, vector<int>, greater<int>> pq;
-
-//         pq.push(intervals[0][1]);
-
-//         for (int i = 1; i < intervals.size(); i++) {
-//             if (intervals[i][0] >= pq.top()) {
-//                 pq.pop();
-//             }
-//             pq.push(intervals[i][1]);
-//         }
-
-//         return pq.size();
-//     }
-// };
-
-
-//扫描线
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        vector<pair<int, int>> events;
+        vector<int> start;
+        vector<int> end;
 
-        for (auto& it : intervals) {
-            events.push_back({it[0], 1});   // start
-            events.push_back({it[1], -1});  // end
+        for (auto meeting : intervals) {
+            start.push_back(meeting[0]);
+            end.push_back(meeting[1]);
         }
 
-        sort(events.begin(), events.end(), [](auto& a, auto& b) {
-            if (a.first == b.first) return a.second < b.second; 
-            return a.first < b.first;
-        });
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
 
-        int count = 0, ans = 0;
-        for (auto& [time, delta] : events) {
-            count += delta;
-            ans = max(ans, count);
+        int i = 0, j = 0, ans = 0, room = 0;
+
+        while (i < start.size()) {
+            if (start[i] < end[j]) {
+                room++;
+                i++;
+            } else {
+                room--;
+                j++;
+            }
+            ans = max(ans, room);
         }
         return ans;
     }
 };
-
-// class Solution {
-// public:
-//     int minMeetingRooms(vector<vector<int>>& intervals) {
-
-//         int n = intervals.size();
-
-//         vector<int> start(n), end(n);
-
-//         for (int i = 0; i < n; i++) {
-//             start[i] = intervals[i][0];
-//             end[i] = intervals[i][1];
-//         }
-
-//         sort(start.begin(), start.end());
-//         sort(end.begin(), end.end());
-
-//         int rooms = 0, maxRooms = 0;
-//         int i = 0, j = 0;
-
-//         while (i < n) {
-//             if (start[i] < end[j]) {
-//                 rooms++;
-//                 i++;
-//             } else {
-//                 rooms--;
-//                 j++;
-//             }
-//             maxRooms = max(maxRooms, rooms);
-//         }
-
-//         return maxRooms;
-//     }
-// };
