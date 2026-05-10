585d0ca2202d7ab2da5c7123ec4ffb8b79508bd6
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-05-10 12:31:46 +1000

    leetcode: 0678-valid-parenthesis-string [Medium] (.cpp) | 05-10-2026 | Time:0 ms (100%) Space:8.1 MB (47.97%)

---
+++ LeetCode/Medium/0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int high = 0, low = 0;
+
+        for (auto& i : s) {
+            if (i == '(') {
+                low++;
+                high++;
+            } else if (i == ')') {
+                low--;
+                high--;
+            } else {
+                low--;
+                high++;
+            }
+
+            if (high < 0) {
+                return false;
+            }
+
+            low = max(low, 0);
+        }
+        return low == 0;
+    }
+};
