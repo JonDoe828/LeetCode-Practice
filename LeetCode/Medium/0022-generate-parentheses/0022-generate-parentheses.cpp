d1122d52939969b1073153b18111174ea798a3a1
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-05-12 14:56:59 +1000

    leetcode: 0022-generate-parentheses [Medium] (.cpp) | 05-12-2026 | Time:3 ms (69.9%) Space:13.5 MB (72.91%)

--- LeetCode/Medium/0022-generate-parentheses/0022-generate-parentheses.cpp
+++ LeetCode/Medium/0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
     string path;
-    int n;
     vector<string> res;
 
-    void backtracking(int left, int right) {
-
+    void backtracking(int n, int left, int right) {
         if (path.size() == n * 2) {
             res.push_back(path);
-            return;
         }
 
         if (left < n) {
             path.push_back('(');
-            backtracking(left + 1, right);
+            backtracking(n, left + 1, right);
             path.pop_back();
         }
 
         if (right < left) {
             path.push_back(')');
-            backtracking(left, right + 1);
+            backtracking(n, left, right + 1);
             path.pop_back();
         }
     }
 
     vector<string> generateParenthesis(int n) {
-        this->n = n;
-        backtracking(0, 0);
+        backtracking(n, 0, 0);
         return res;
     }
 };
