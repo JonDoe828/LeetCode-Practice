fefa295b73475edc04380cc213ce1aee37f7daa1
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-05-17 16:16:36 +1000

    leetcode: 0242-valid-anagram [Easy] (.cpp) | 05-17-2026 | Time:0 ms (100%) Space:9.6 MB (74.94%)

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.cpp
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if (s.length() != t.length())
+            return false;
+
         vector<int> ans(26, 0);
 
-        for (auto c : s) {
+        for (auto& c : s) {
             ans[c - 'a']++;
         }
 
-        for (auto c : t) {
+        for (auto& c : t) {
             ans[c - 'a']--;
         }
 
-        for (auto c : ans) {
+        for (auto& c : ans) {
             if (c != 0) {
                 return false;
             }
