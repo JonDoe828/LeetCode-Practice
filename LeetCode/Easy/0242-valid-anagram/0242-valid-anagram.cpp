29be738d95822644dfc959c07fef3f82580cdaeb
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-05-17 16:14:34 +1000

    leetcode: 0242-valid-anagram [Easy] (.cpp) | 05-17-2026 | Time:3 ms (45.48%) Space:9.7 MB (48.41%)

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.cpp
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length())
-            return false;
+        vector<int> ans(26, 0);
 
-        vector<int> count(26, 0); // 用数组代替哈希表，只考虑小写字母 a-z
-
-        for (int i = 0; i < s.length(); ++i) {
-            count[s[i] - 'a']++; // s 中的字符加一
-            count[t[i] - 'a']--; // t 中的字符减一
+        for (auto c : s) {
+            ans[c - 'a']++;
         }
 
-        for (int c : count) {
-            if (c != 0)
-                return false; // 有字符数量不同
+        for (auto c : t) {
+            ans[c - 'a']--;
+        }
+
+        for (auto c : ans) {
+            if (c != 0) {
+                return false;
+            }
         }
 
         return true;
