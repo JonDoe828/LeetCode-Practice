db02b973aa84b0ede340e2de4344e3d0a8ccd08d
Haozhe Hu <89440644+JonDoe828@users.noreply.github.com>
2026-06-13 00:05:13 +1000

    leetcode: 0845-longest-mountain-in-array [Medium] (.cpp) | 06-13-2026 | Time:4 ms (13.21%) Space:22.3 MB (80.62%)

--- LeetCode/Medium/0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
+++ LeetCode/Medium/0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int longestMountain(vector<int>& arr) {
         int ans = 0;
-
+        //注意边界
         for (int i = 1; i < arr.size() - 1; i++) {
             if (arr[i - 1] < arr[i] && arr[i] > arr[i + 1]) {
                 int left = i;
