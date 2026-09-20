edcada9e4c6a58951397a0132a8b29e25b6e8278
JonDoe828 <89440644+JonDoe828@users.noreply.github.com>
2026-09-20 20:50:45 +1000

    leetcode: 0191-number-of-1-bits [Easy] (.cpp) | 09-20-2026 | Time:0 ms (100%) Space:8.2 MB (48.04%)

--- LeetCode/Easy/0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ LeetCode/Easy/0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,9 +3,7 @@ public:
     int hammingWeight(int n) {
         int count = 0;
         while (n != 0) {
-            n & 1;
-
-            if (n & 1 == 1) {
+            if ((n & 1) == 1) {
                 count++;
             }
             n = n >> 1;
