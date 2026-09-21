ca478ef5854c7c5af087096050a60f47307828fb
JonDoe828 <89440644+JonDoe828@users.noreply.github.com>
2026-09-22 02:58:16 +1000

    leetcode: 0066-plus-one [Easy] (.cpp) | 09-22-2026 | Time:0 ms (100%) Space:11.6 MB (25.82%)

---
+++ LeetCode/Easy/0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+
+        for (int i = n - 1; i >= 0; i--) {
+            digits[i]++;
+
+            if (digits[i] < 10) {
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        digits[0] = 1;
+        digits.push_back(0);
+
+        return digits;
+    }
+};
