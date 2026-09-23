749f51558d4d21c6781a5dbcbbb101e8f8bce8a5
JonDoe828 <89440644+JonDoe828@users.noreply.github.com>
2026-09-24 01:40:40 +1000

    leetcode: 0287-find-the-duplicate-number [Medium] (.cpp) | 09-24-2026 | Time:8 ms (46.49%) Space:65.1 MB (69.72%)

--- LeetCode/Medium/0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ LeetCode/Medium/0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
+    // int findDuplicate(vector<int>& nums) {
+
+    //     int lo = 1, n = nums.size() - 1; // 值域是 1 到 n
+    //     while (lo < n) {
+    //         int mid = lo + (n - lo) / 2;
+
+    //         int cnt = 0;
+    //         for (int x : nums) {
+    //             if (x <= mid)
+    //                 cnt++;
+    //         }
+
+    //         if (cnt > mid) {
+    //             n = mid;
+    //         } else {
+    //             lo = mid + 1;
+    //         }
+    //     }
+    //     return lo;
+    // }
+
     int findDuplicate(vector<int>& nums) {
+        int slow = 0, fast = 0;
 
-        int lo = 1, n = nums.size() - 1; // 值域是 1 到 n
-        while (lo < n) {
-            int mid = lo + (n - lo) / 2;
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
 
-            int cnt = 0;
-            for (int x : nums) {
-                if (x <= mid)
-                    cnt++;
-            }
-
-            if (cnt > mid) {
-                n = mid;
-            } else {
-                lo = mid + 1;
+            if (slow == fast) {
+                int index1 = 0, index2 = slow;
+                while (index1 != index2) {
+                    index1 = nums[index1];
+                    index2 = nums[index2];
+                }
+                return index1;
             }
         }
-        return lo;
     }
 };
