83173318265d876e573de38a29c69691681e4944
JonDoe828 <89440644+JonDoe828@users.noreply.github.com>
2026-09-25 20:35:18 +1000

    leetcode: 0399-evaluate-division [Medium] (.cpp) | 09-25-2026 | Time:0 ms (100%) Space:12.1 MB (25.37%)

---
+++ LeetCode/Medium/0399-evaluate-division/0399-evaluate-division.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    unordered_map<string, vector<pair<string, double>>> graph;
+
+    vector<double> calcEquation(vector<vector<string>>& equations,
+                                vector<double>& values,
+                                vector<vector<string>>& queries) {
+
+        vector<double> ans;
+        // 1. 建图
+        for (int i = 0; i < equations.size(); i++) {
+            string a = equations[i][0];
+            string b = equations[i][1];
+            double val = values[i];
+
+            graph[a].push_back({b, val});
+            graph[b].push_back({a, 1.0 / val});
+        }
+        // 2. 一个个处理 query
+        for (int i = 0; i < queries.size(); i++) {
+            string start = queries[i][0];
+            string end = queries[i][1];
+
+            if (graph.find(start) == graph.end() ||
+                graph.find(end) == graph.end()) {
+                ans.push_back(-1.0);
+                continue;
+            }
+
+            unordered_set<string> visited;
+
+            double res = dfs(start, end, 1.0, visited);
+
+            ans.push_back(res);
+        }
+
+        return ans;
+    }
+
+    double dfs(string cur, string target, double product,
+               unordered_set<string>& visited) {
+        if (cur == target)
+            return product;
+
+        visited.insert(cur);
+
+        for (auto& [next, weight] : graph[cur]) {
+
+            if (visited.count(next))
+                continue;
+
+            double res = dfs(next, target, product * weight, visited);
+
+            if (res != -1.0)
+                return res;
+        }
+
+        return -1.0;
+    }
+};
